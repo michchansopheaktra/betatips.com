5d58878b54f1b4906ae1a03ef186147b58b617f9
hello <hello@hellos-MacBook-Pro-2.local>
2025-08-12 20:06:27 +0700

    delete database

--- blog/settings.py
+++ blog/settings.py
@@ -90,26 +90,33 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 #     }
 # }
 
-DB_LIVE = os.getenv("DB_LIVE")
-
-if DB_LIVE in ["False", False]:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-            }
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',  # Change to mysql/sqlite if needed
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
+# DB_LIVE = os.getenv("DB_LIVE")
+
+# if DB_LIVE in ["False", False]:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#             }
+#         }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',  # Change to mysql/sqlite if needed
+#             'NAME': os.getenv('DB_NAME'),
+#             'USER': os.getenv('DB_USER'),
+#             'PASSWORD': os.getenv('DB_PASSWORD'),
+#             'HOST': os.getenv('DB_HOST'),
+#             'PORT': os.getenv('DB_PORT'),
+#         }
+#     }
 
 
 # Password validation
